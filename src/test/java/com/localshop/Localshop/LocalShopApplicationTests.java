3c548844df63cff03d335f2ab657409868023053
FranxOpp <coppolafrancesco303@gmail.com>
2024-05-09 19:46:27 +0200

    First Commit Project

---
+++ src/test/java/com/localshop/Localshop/LocalShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localshop.Localshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
